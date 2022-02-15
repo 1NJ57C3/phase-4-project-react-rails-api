5109a70b31d9af2636dc24992e461a9f43718479
Lance Hebert <LSUHEBERT@gmail.com>
2022-02-14 22:53:17 -0800

    Auth built, input styling for forms

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,18 @@
 class ApplicationController < ActionController::API
+    before_action :authorize
     include ActionController::Cookies
-
-    def hello_world
-        session[:count] = (session[:count] || 0) + 1
-        render json: { count: session[:count] }
+    rescue_from ActiveRecord::RecordInvalid, with: :response_unprocessable_entity
+    
+  
+  
+  private  
+  
+    def authorize
+      User.find_by(id: session[:user_id])     
+      render json: {errors: ["Not Authorized"]}, status: 401 unless session.include? :user_id
     end
+  
+    def response_unprocessable_entity(e)
+      render json: {errors: e.record.errors.full_messages},status: 422
+    end  
 end
