45a53cb1372ec98c82a620b8cef4d4630b6b76f9
Lance Hebert <LSUHEBERT@gmail.com>
2022-02-16 00:34:47 -0800

    Added display of Characters to CharMenu, Added Delete button with Delete functionality to CharMenu, Added CharCard component

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,5 +1,5 @@
 class Character < ApplicationRecord
   belongs_to :user
-  has_many :character_equipment
+  has_many :character_equipment,dependent: :destroy
   has_many :equipment, through: :character_equipment
 end
--- client/src/pages/CharMenu.js
+++ client/src/pages/CharMenu.js
@@ -1,34 +1,41 @@
-import React from 'react'
-import { useState,useEffect } from "react";
+import React from "react";
+import { useState, useEffect } from "react";
 import { Button } from "../styles";
+import CharCard from "../components/CharCard";
 
 function CharMenu() {
-  const [charHolder,setCharHolder]=useState([])
+  const [charHolder, setCharHolder] = useState([]);
 
-  // useEffect(() => {
-  //   fetch("grab all user characters /characters")
-  //   .then(r => r.json()).then(charList => )
-  // },[])
+  function handleDelete(char) {
+    setCharHolder(charHolder.filter((c) => c.id !== char.id));
+  }
+
+  useEffect(() => {
+    fetch("http://localhost:4000/characters")
+      .then((r) => r.json())
+      .then((charList) => setCharHolder(charList));
+  }, []);
+
+  const displayCharList = charHolder.map((char) => {
+    return (
+      <CharCard key={char.char_name} char={char} handleDelete={handleDelete} />
+    );
+  });
 
   // useEffect fetch to grab all currently logged in users characters
   //set to state
-  //map through charList and display, <Character props  /> 
+  //map through charList and display, <Character props  />
 
   //dagger throwing animation, CSS bigger on mouseover of Character card
 
-
-
-
-
-
-
-
   return (
     <>
-    <div><Button > New Character </Button> </div>
+      <div>
+        <Button> New Character </Button>{" "}
+      </div>
+      <div>{displayCharList}</div>
     </>
-  )
+  );
 }
 
-export default CharMenu
-
+export default CharMenu;
