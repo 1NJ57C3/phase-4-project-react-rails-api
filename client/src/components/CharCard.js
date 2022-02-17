7b8e37792cffbc3ec4797f7109cd757f1afe4026
Lance Hebert <LSUHEBERT@gmail.com>
2022-02-17 12:04:47 -0800

    Added routing for charmenu to charSpec and charCreate to charSpec,Added Choose button for charMenu

--- client/src/components/CharCard.js
+++ client/src/components/CharCard.js
@@ -1,19 +1,26 @@
 import React from "react";
 import { Button, Box } from "../styles";
+import { Link, useNavigate } from "react-router-dom";
 
-function CharCard({ char, handleDelete }) {
-  function clickHandler() {
-    fetch(`/characters/${char.id}`,{
+function CharCard({ char, handleDelete, setCharacterScreen }) {
+  let navigate = useNavigate();
+
+  function clickDelete() {
+    fetch(`/characters/${char.id}`, {
       method: "DELETE",
-    })    
-      .then(() => handleDelete(char));
+    }).then(() => handleDelete(char));
   }
+  function clickSelect() {
+    navigate(`/characters/${char.id}`);
+  }
+
   return (
-    
     <div>
-         <Box>
-      <Button onClick={clickHandler}>Delete</Button>
-    {char.char_name} the {char.job} </Box> 
+      <Box>
+        <Button onClick={clickDelete}>Delete</Button>
+        {char.char_name} the {char.job}
+        <Button onClick={clickSelect}> Choose</Button>
+      </Box>
     </div>
   );
 }
