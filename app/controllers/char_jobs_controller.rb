00a277f9478310e8db7ff42e15926013c328c8f2
1NJ57C3 <85198277+1NJ57C3@users.noreply.github.com>
2022-02-18 09:21:17 -0800

    Client/User Interface Updates! - Updated `CharCreateForm` visually 	- Form content centered, margined, and padded 	- Increment/Decrement buttons have been spaced out with CSS to near uniform spacing. Needs a little more love. 	- Added background image and applied some styling (might scrap) 	- Darkened font colors beneath header for readability 	- Changed `submit` input to Button - Updated button styles across entire app - Adjusted margins on our various `Button`s - Adjusted some CSS in our `styled-component`s - Moved non-`styled-components` CSS into `index.css` - Added slightly more descriptive comments to couple lines in back end

--- app/controllers/char_jobs_controller.rb
+++ app/controllers/char_jobs_controller.rb
@@ -1,4 +1,6 @@
 class CharJobsController < ApplicationController
+    # pre-built for stretch goals/later refactor
+
     # def index
         # render json: # * character, job
     # end
--- client/src/styles/Button.js
+++ client/src/styles/Button.js
@@ -3,12 +3,16 @@ import styled from "styled-components";
 const Button = styled.button`
   cursor: pointer;
   font-size: 1rem;
-  border: 1px solid transparent;
-  border-radius: 6px;
+  /* border: 1px solid transparent; */
+  border: 1px outset transparent;
+  /* border-radius: 6px; */
+  border-radius: 7px;
   text-decoration: none;
-  margin-right: 1vh;
+  margin-right: .5vh;
+  margin-left: .5vh;
   margin-top: .7vh;
-  background-color: #810000;
+  background-image: linear-gradient(to bottom right, rgba(200,0,0,1), rgba(81,0,0,1));
+  /* background-color: #810000; */
   color: lightgrey;
 `;
 
