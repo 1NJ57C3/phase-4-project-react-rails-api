947c0e4f49eb59839dde0678330bb413ab5d5eef
Lance Hebert <LSUHEBERT@gmail.com>
2022-03-17 19:30:13 -0700

    make it better

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,6 @@ class UsersController < ApplicationController
     private
 
     def user_params
-        params.permit(:username,:password)
+        params.permit(:username,:password,:password_confirmation)
     end
 end
